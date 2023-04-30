c524d9a95cc9d93291aa94913841658f2a4e0360
Masud Abdullah <abdullahtamim885@gmail.com>
2023-05-01 04:11:43 +0600

    Math Function

--- function_4.c
+++ function_4.c
@@ -1,17 +1,17 @@
-//No return type + Np parameter
-#include<stdio.h>
+// No return type + Np parameter
+#include <stdio.h>
 void sum()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    int ans=a+b;
-    printf("%d\n",ans);
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int ans = a + b;
+    printf("%d\n", ans);
 }
 int main()
 {
     sum();
     sum();
     sum();
-    //If I want then i can call any function several time.    
+    // If I want then i can call any function several time.
     return 0;
 }
